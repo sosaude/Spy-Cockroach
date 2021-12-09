7409704b07ccf542fef094b45aa90b2f6d5ec257
farioso-ferando <farioso.f@gmail.com>
2021-12-10 00:53:47 +0200

    Arquivos iniciais e logica

---
+++ main.py
@@ -0,0 +1,43 @@
+"""spy cockroach"""
+
+# Importando o modulo os.
+import os
+
+# Pegando o caminho raiz dos arquivos como um: "/"
+PATH = "/home/farioso/www/file03_git/fluentui-system-icons/assets"
+
+# Indo para a raiz: "/". 
+# Nota: Quando digo raiz, quero dizer o diretorio principal ou pai que contém seus arquivos.
+os.chdir(PATH)
+
+# Pegando a lista de diretorios ou arquivos existentes na raiz.
+# A "mother_folder" é uma lista de arquivos ou pastas filhas dela. Ex: ["path1", "path2", "path3"...]
+mother_folder = os.listdir(".")
+
+# Filtrando quantos dados quero que seja retornado.
+filter = 0
+
+# Varrendo a lista de arquivos na pasta mãe para a variavel path.
+for path in mother_folder:
+    
+    filter+=1
+    
+    # Definindo a quantidade de resultados que serão retornados.
+    # Quando a condição é atendida, o "filter" é reiniciado para zero e a listagem é abortada.
+    if filter>=10:
+        filter = 0
+        break
+    else:
+        try:
+            # Pegando as "paths" existentes em cada path.
+            subfolders = (os.listdir(path))
+
+            # Gerando a lista de arquivos.
+            # Depois de gerada a lista, corremos a mesma para a variavel "sigle_file" que retorna o que está dentro dela.
+            file_list = [file.name for file in os.scandir(PATH +"/"+path+"/"+subfolders[0]) if file.is_file()]
+            for single_file in file_list:
+                print(single_file)
+        
+        # Capturando erro.
+        except os.error as exceptError:
+            print("Erro: {}".format(exceptError))
